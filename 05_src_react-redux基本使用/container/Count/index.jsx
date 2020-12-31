6f12bf16fa1bbcc6417b2fcb164692eeb2be5cb8
tianyu5177 <zhangtianyu5177@sina.com>
2020-12-31 10:16:07 +0800

    更新

--- 05_src_react-redux基本使用/container/Count/index.jsx
+++ 05_src_react-redux基本使用/container/Count/index.jsx
@@ -11,20 +11,20 @@ import {
 //引入connect，用于：连接UI与redux、生成容器组件
 import {connect} from 'react-redux'
 
-/* a函数的返回值必须是一个对象
+/* mapStateToProps函数的返回值必须是一个对象
 返回的这个对象的key就作为传递给UI组件的props的key
 返回的这个对象的value就作为传递给UI组件的props的value
-a函数主要用于给UI组件传递状态 */
+mapStateToProps函数主要用于给UI组件传递状态 */
 function mapStateToProps(state){
 	return {
 		sum:state
 	}
 }
 
-/* b函数的返回值必须是一个对象
+/* mapDispatchToProps函数的返回值必须是一个对象
 返回的这个对象的key就作为传递给UI组件的props的key
 返回的这个对象的value就作为传递给UI组件的props的value
-b函数主要用于给UI组件传递操作状态的方法 */
+mapDispatchToProps函数主要用于给UI组件传递操作状态的方法 */
 function mapDispatchToProps(dispatch){
 	return {
 		jia: value => dispatch(createIncrementAction(value)),
