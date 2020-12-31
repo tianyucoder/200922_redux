3041ba94c0d491b894a498ed986b80e5d2934297
tianyu5177 <zhangtianyu5177@sina.com>
2020-12-31 10:53:51 +0800

    更新

--- 05_src_react-redux基本使用/redux/constant.js
+++
@@ -1,3 +0,0 @@
-//该模块是存储action对象中type的值，目的怕程序员写错单词
-export const INCREMENT = 'increment'
-export const DECREMENT = 'decrement'
--- src/container/Count/index.jsx
+++ src/container/Count/index.jsx
@@ -4,23 +4,29 @@ import {
 	createDecrementAction,
 	createIncrementAsyncAction
 } from '../../redux/count_action'
-
 import {connect} from 'react-redux'
 
-function mapStateToProps(state){
-	return {
-		sum:state
+//普通写法
+/* const mapDispatchToProps = dispatch => ({
+	jia: value => dispatch(createIncrementAction(value)),
+	jian: value => dispatch(createDecrementAction(value)),
+	jiaAsync: (value,time) => dispatch(createIncrementAsyncAction(value,time))
+}) */
+
+//精简写法
+/* const mapDispatchToProps = {
+	jia:createIncrementAction,
+	jian:createDecrementAction,
+	jiaAsync:createIncrementAsyncAction
+} */
+
+
+export default connect(
+	state => ({sum:state}), //映射状态
+	//映射操作状态的方法
+	{
+		jia:createIncrementAction,
+		jian:createDecrementAction,
+		jiaAsync:createIncrementAsyncAction
 	}
-}
-
-function mapDispatchToProps(dispatch){
-	return {
-		jia: value => dispatch(createIncrementAction(value)),
-		jian: value => dispatch(createDecrementAction(value)),
-		jiaAsync: (value,time) => dispatch(createIncrementAsyncAction(value,time)),
-	}
-}
-
-const CountContainer = connect(mapStateToProps,mapDispatchToProps)(CountUI)
-
-export default CountContainer
+)(CountUI)
